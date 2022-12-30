840f8adc415fb465cdba28a3abfb13fe336868d5
hadikp <hp20001025@gmail.com>
2022-12-30 20:49:41 +0100

    first commit

---
+++ src/main/java/bluesight/BlueSightCanbanApplication.java
@@ -0,0 +1,13 @@
+package bluesight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueSightCanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueSightCanbanApplication.class, args);
+	}
+
+}
