2365121693c49ad8eb27c0671e571bc0c44d0915
hadikp <hp20001025@gmail.com>
2022-12-30 21:21:02 +0100

    pom.xml, projekt start

--- src/main/java/bluesight/BlueSightCanbanApplication.java
+++ src/main/java/bluesight/BlueSightCanbanApplication.java
@@ -1,13 +1,20 @@
 package bluesight;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlueSightCanbanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlueSightCanbanApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper createModelMapper(){
+		return new ModelMapper();
 	}
 
 }
