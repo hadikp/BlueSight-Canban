3eb4c13161d19aaa07f3ee6bf81182121e194df0
hadikp <hp20001025@gmail.com>
2023-01-19 16:37:17 +0100

    swimlane,squad,column table add new datas

--- src/main/resources/db/migration/V2__squad.sql
+++ src/main/resources/db/migration/V2__squad.sql
@@ -1,4 +1,3 @@
 create table squads(id bigint auto_increment PRIMARY KEY, name varchar(50), description varchar(150), start_date date, board_id bigint);
-insert into squads(name, description, start_date, board_id) values ('squad1', 'Description of squad1', '2022-12-12', 1);
-insert into squads(name, description, start_date, board_id) values ('squad2', 'Description of squad2', '2022-12-14', 2);
-insert into squads(name, description, start_date, board_id) values ('squad3', 'Description of squad3', '2022-12-15', 3);
+insert into squads(name, description, start_date, board_id) values ('First squad', 'Description of first squad', '2022-12-12', 1);
+insert into squads(name, description, start_date, board_id) values ('My test squad', 'Description of my test squad', '2023-01-14', 2);
