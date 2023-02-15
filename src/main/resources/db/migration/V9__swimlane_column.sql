33e6fea1d082cedf2d229bb2b637270ace726e79
hadikp <hp20001025@gmail.com>
2023-02-15 20:01:36 +0100

    New cards sql datas

--- src/main/resources/db/migration/V9__swimlane_column.sql
+++ src/main/resources/db/migration/V9__swimlane_column.sql
@@ -1,2 +1,9 @@
 create table swimlane_column(swimlane_id bigint, column_id bigint);
 insert into swimlane_column(swimlane_id, column_id) values (1, 1);
+insert into swimlane_column(swimlane_id, column_id) values (1, 2);
+insert into swimlane_column(swimlane_id, column_id) values (1, 3);
+insert into swimlane_column(swimlane_id, column_id) values (1, 4);
+insert into swimlane_column(swimlane_id, column_id) values (2, 1);
+insert into swimlane_column(swimlane_id, column_id) values (2, 2);
+insert into swimlane_column(swimlane_id, column_id) values (2, 3);
+insert into swimlane_column(swimlane_id, column_id) values (2, 4);
